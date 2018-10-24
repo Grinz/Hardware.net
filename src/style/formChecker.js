0c4ee6a9876937020a7f9488f1380248f40b9def
Solène MARIE <solenemarie67@gmail.com>
2018-10-24 16:40:23 +0200

    Finalized signup form

--- src/style/formChecker.js
+++ src/style/formChecker.js
@@ -1,26 +1,37 @@
-function checkEmptyFields(form) {
-    if (form.firstName.value == "" || myform.myname.value == null)
-    {
-        alert("Name is mandatory");
-        return false;
-    }
-    
-    if (myform.phoneNumber.value == "" || myform.phoneNumber.value == null)
-    {
-        alert("Phone number is mandatory");
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
 $(document).ready(function() {
-    $("#formValidate").validate({
+    $("#signupForm").validate({
         rules: {
             firstName: {
                 required: true,
-                minlength: 5
+                minlength: 3
+            },
+            lastName: {
+                required: true,
+                minlength: 3
+            },
+            phoneNumber: {
+                required: true,
+                minlength: 10,
+                maxlength: 10,
+                digits: true
+            },
+            address: {
+                required: true,
+                minlength: 10
+            },
+            city: {
+                required: true,
+                minlength: 3,
+            },
+            zipCode: {
+                required: true,
+                minlength: 5,
+                maxlength: 5,
+                digits: true
+            },
+            emailAddress: {
+                required: true,
+                email: true
             },
             password: {
                 required: true,
@@ -34,19 +45,59 @@ $(document).ready(function() {
         },
         //For custom messages
         messages: {
-            firstName:{
-                required: "Enter a username",
-                minlength: "Enter at least 5 characters"
+            firstName: {
+                required: "Veuillez saisir un prénom.",
+                minlength: "La saisie doit être de 3 caractères de long minimum."
+            },
+            lastName: {
+                required: "Veuillez saisir un nom de famille.",
+                minlength: "La saisie doit être de 3 caractères de long minimum."
+            },
+            phoneNumber: {
+                required: "Veuillez saisir un numéro de téléphone.",
+                minlength: "La saisie doit être de 10 caractères de long.",
+                maxlength: "La saisie doit être de 10 caractères de long.",
+                digits: "Seuls des chiffres peuvent être saisis."
+            },
+            address: {
+                required: "Veuillez saisir une adresse postale.",
+                minlength: "La saisie doit être de 10 caractères de long minimum."
+            },
+            city: {
+                required: "Veuillez saisir une ville.",
+                minlength: "La saisie doit être de 3 caractères de long minimum."
+            },
+            zipCode: {
+                required: "Veuillez saisir un code postal.",
+                minlength: "La saisie doit être de 5 caractères de long.",
+                maxlength: "La saisie doit être de 5 caractères de long.",
+                digits: "Seuls des chiffres peuvent être saisis."
+            },
+            emailAddress: {
+                required: "Veuillez saisir une adresse e-mail.",
+                email: "Le format de l'adresse e-mail est invalide."
+            },
+            password: {
+                required: "Veuillez saisir un mot de passe.",
+                minlength: "La saisie doit être de 5 caractères de long minimum."
+            },
+            passwordConfirmation: {
+                required: "Veuillez re-saisir votre mot de passe.",
+                minlength: "La saisie doit être de 5 caractères de long minimum.",
+                equalTo: "Les deux saisies de mot de passe ne correspondent pas."
             }
         },
         errorElement : 'div',
+        errorClass: 'invalid',
+        validClass: 'valid',
         errorPlacement: function(error, element) {
-          var placement = $(element).data('error');
-          if (placement) {
-            $(placement).append(error)
-          } else {
-            error.insertAfter(element);
-          }
+            var placement = $(element).data('error');
+
+            if (placement) {
+                $(placement).append(error)
+            } else {
+                error.insertAfter(element);
+            }
         }
      });
 });
